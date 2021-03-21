275e4550af30e67c459e7e4c79717198df6babe5
saravana-ss <mails2saravana@gmail.com>
2021-03-22 00:14:41 +0530

    Completed modules4

--- jobs/app.py
+++ jobs/app.py
@@ -10,7 +10,8 @@ def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
-    connection.row_factoy = sqlite3.Row
+    connection.row_factory = sqlite3.Row
+
     return connection
 
 
